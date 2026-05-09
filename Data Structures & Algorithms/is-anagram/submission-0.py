ae63a4261798dcdee99c975a476a57f08f991181
Saniya <74100242+Saniiyaa59@users.noreply.github.com>
2026-05-09 11:09:36 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #two strings are anagrams if the alphabet arrays are equal
+
+        arr1 = [0]*26
+        for ch in s:
+            index = ord(ch)-97
+            arr1[index] += 1
+
+        arr2 = [0]*26
+        for ch in t:
+            index = ord(ch)-97
+            arr2[index]+=1
+
+        return str(arr1) == str(arr2)
