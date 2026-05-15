8b634c039fdfbb00241bfd951a4f7a8bb0eb4c7a
Saniya <74100242+Saniiyaa59@users.noreply.github.com>
2026-05-15 15:19:09 -0400

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            res += str(len(s)) + "*"
+            res+=s
+
+        print(res)
+        return res
+
+
+    def decode(self, s: str) -> List[str]:
+
+        res = []
+        l = 0
+        while l < len(s):
+
+            dig = ""
+            while  l < len(s) and s[l] >= "0" and s[l] <= "9":
+                dig+=s[l]
+                l+=1
+
+            l+=1 #skip *
+            d = int(dig)
+            stop = l+d
+            
+            temp = ""
+            while l < stop:
+                temp += s[l]
+                l+=1
+
+            res.append(temp)
+
+        return res
+
+
+
+
+
+        return res
